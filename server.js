3563cd27eb772fe59a465b7715c8f6b4d4901b78
lumip06 <lumipitac@gmail.com>
2024-07-29 11:19:58 +0300

    added nodejs

--- server.js
+++ server.js
@@ -3,9 +3,8 @@ const path = require('path'); // Add path module to work with file paths
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'main-page.html')); // Send the HTML file
-});
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
